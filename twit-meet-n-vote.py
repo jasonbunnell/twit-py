69b034a870643f1328792af564eb768f4e12b751
bunnell <jason.bunnell@gmail.com>
2018-08-04 18:04:42 -0400

    added key and secret for Twitter API

--- twit-meet-n-vote.py
+++ twit-meet-n-vote.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import textblob
 
-consumer_key = ''
-consumer_secret = ''
+consumer_key = 'lOQqd21ewxvS4AOniUPOIQ'
+consumer_secret = 'euI5dSqInMaGy1z3YWrX0cPTR9BxwXaVwmnsFI2N0kc'
 
-access_token = ''
-access_token secret = ''
+access_token = '14446071-yoW0YpbBqbNVfenvPvh9huq2uMest0bg57PdRhvHX'
+access_token secret = 'eT6a74X2OjYRmy4NLZlWkDJccyWCu0VuSqGJQIEvrtLqi'
 
 auth = tweepy.OAuthHandler(consumer_key )
 
