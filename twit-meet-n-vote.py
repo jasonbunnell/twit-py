42a89bb869c8a915fc9dd5b8522b7acc355bc9d7
jasonbunnell <jason.bunnell@gmail.com>
2018-08-04 16:57:47 -0500

    Python Twitter first GitHub commit

---
+++ twit-meet-n-vote.py
@@ -0,0 +1,14 @@
+import tweepy
+from textblob import textblob
+
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key )
+
+## TODO list
+# follow user using set hashtag
+# vote on speaker vie there user handle
